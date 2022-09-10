862faca47168186f7c08dda232fa7dbfe7869203
Daniel Lovera <daanilo2497@gmail.com>
2022-09-10 16:52:01 -0300

    Refactor add error handling

--- src/config/logging.py
+++ src/config/logging.py
@@ -1,8 +1,15 @@
 # Standard library imports
+import logging
 from yaml import safe_load
 from logging.config import dictConfig
 
-
 def set_config():
-    dict_config = safe_load(open("src/config/logging.yaml", "r"))
-    dictConfig(dict_config)
+    FPATH = "src/config/logging.yaml"
+    FMODE = "r"
+
+    try:
+        with open(FPATH, FMODE) as file:
+            dict_config = safe_load(file)
+            dictConfig(dict_config)
+    except OSError as error:
+        logging.error(f"{error=}, {type(error)=}")
