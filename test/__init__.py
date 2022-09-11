15dd3c6b671c46309314391d5b3bf5478b79f91f
marconcz <marconcz@gmail.com>
2022-09-11 15:26:45 -0300

    Added example test

---
+++ test/__init__.py
@@ -0,0 +1,3 @@
+# Adds higher directory to python modules path.
+import sys
+sys.path.append("..") 
---
+++ test/trip_test.py
@@ -0,0 +1,13 @@
+# Third party imports
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+# Local application imports
+from src.router.trips import router
+
+client = TestClient(router)
+
+
+def test_main_route():
+    response = client.get("/")
+    assert response.json() == {"message": "Hey, It is me fastapi"}
