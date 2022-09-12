56efa28133f27ccb105fea7708cd65cf3db85e05
marconcz <marconcz@gmail.com>
2022-09-12 19:33:47 -0300

    Added 2 examples with GeoPY in router/trips.py and Added GeoPy dependency

--- src/router/trips.py
+++ src/router/trips.py
@@ -1,5 +1,10 @@
 # Third party imports
-from fastapi import APIRouter
+from fastapi import APIRouter 
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+geolocator = Nominatim(user_agent="fiuber")
+# Define constants
+PRICE_PER_KILOMETER = 5
 
 router = APIRouter()
 
@@ -7,3 +12,14 @@ router = APIRouter()
 @router.get("/")
 async def main_route():
     return {"message": "Hey, It is me fastapi"}
+
+#Example: Transform an Address into Coordinates (Latitude, Longitude)
+@router.get("/destination/address")
+async def destination_with_address(address: str):
+    location = geolocator.geocode(address)
+    return {"latitude": location.latitude,"longitude": location.longitude}
+#Example: Received Coordinates (Latitude, Longitude)
+#Return: Distance(geodesic) between Coordinates received
+@router.get("/price")
+async def price_calculator(latitude: float, longitude: float, latitude2: float, longitude2: float):
+    return {"price": PRICE_PER_KILOMETER*(geodesic((latitude,longitude),(latitude2,longitude2)).kilometers)}
