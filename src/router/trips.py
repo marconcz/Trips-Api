2a5fc985d2ba4e7a2b8ba61db5479620b3facf1f
marconcz <marconcz@gmail.com>
2022-10-12 18:25:52 -0300

    Cambiado el calculo del precio a distancia por KM

--- src/router/trips.py
+++ src/router/trips.py
@@ -1,10 +1,12 @@
 # Third party imports
 from fastapi import APIRouter 
-from geopy.geocoders import Nominatim
-from geopy.distance import geodesic
-geolocator = Nominatim(user_agent="fiuber")
+from fastapi.encoders import jsonable_encoder
+#from service.price_rules import driver, passenger, trip, rule
+from fastapi.encoders import jsonable_encoder
 # Define constants
 PRICE_PER_KILOMETER = 5
+#Global Variable RULE
+
 
 router = APIRouter()
 
@@ -14,12 +16,23 @@ async def main_route():
     return {"message": "Hey, It is me fastapi"}
 
 #Example: Transform an Address into Coordinates (Latitude, Longitude)
-@router.get("/destination/address")
-async def destination_with_address(address: str):
-    location = geolocator.geocode(address)
-    return {"latitude": location.latitude,"longitude": location.longitude}
-#Example: Received Coordinates (Latitude, Longitude)
-#Return: Distance(geodesic) between Coordinates received
-@router.get("/price")
-async def price_calculator(latitude: float, longitude: float, latitude2: float, longitude2: float):
-    return {"price": PRICE_PER_KILOMETER*(geodesic((latitude,longitude),(latitude2,longitude2)).kilometers)}
+# @router.get("/destination-address")
+# async def destination_with_address(address: str):
+#     location = geolocator.geocode(address)
+#     return {"latitude": location.latitude,"longitude": location.longitude}
+
+#Example: Received travel distance
+#Return: Trip price calculated with distance multiplied PRICE_PER_KILOMETER
+@router.get("/trip-price")
+async def price_calculator(distance: float):
+    return {"price": PRICE_PER_KILOMETER*distance}
+#
+
+
+#Example: Receiver (passenger_stats, driver_stats, trip_stats)
+#Return: Trip price@router.get("/trip-price")
+# @router.post("/trip-price-with-rules")
+# async def price_calculator(driver_stats: driver, client_stats: passenger, trip_stats: trip):
+#     price = rule({driver.json()}).calculate_price()
+
+#     return {"price": price}
