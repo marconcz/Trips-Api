6f311eaa62c526da89754a63d07e10d84abf616f
marconcz <marconcz@gmail.com>
2022-12-03 23:34:36 -0300

    Cambio de base postgressql

--- src/repository/secret.py
+++ src/repository/secret.py
@@ -1,5 +1,5 @@
-host = "ec2-44-196-223-128.compute-1.amazonaws.com"
-database = "d2lvvdf87dltu3"
-user = "ugqfctwfstfsmb"
-password = "f8ae8889097ef61449ff8ebf040a341694d01d958f49184f5ba17b1dac7a0a18"
+host = "ec2-107-22-122-106.compute-1.amazonaws.com"
+database = "d34vb90jvgha4s"
+user = "lzcwgkhhteprmw"
+password = "228caeba6ceab6638e8e0d44087894881000879c4f1c80c66ff04584968c296b"
 port = 5432
